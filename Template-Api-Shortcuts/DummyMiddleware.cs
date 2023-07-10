b6306b1b4549bddde4d7a372b71bfac73147315e
Olivier Zaradny <ozaradny@outlook.com>
2023-07-10 13:38:01 +0200

    Initial commit
    init

---
+++ Template-Api-Shortcuts/DummyMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Template_Api_Shortcuts
+{
+    public class DummyMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DummyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("The Dummy Middleware is invoked");
+            await _next(context);
+        }
+    }
+}
+
